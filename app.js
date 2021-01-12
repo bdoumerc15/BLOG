b05c3e0faf9b85009ff91abe73093b9dc49787e6
bladimir doumerc <bdoumerc15@gmail.com>
2021-01-12 13:01:30 -0700

    Se han agregadp archivos .js y se comprende mejor cómo enviar archivos hacia el servidor por medio de app.get('/'....)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 import express from "express"
 import bodyParser from "body-parser"
+import { title } from "./public/javascript/home.js"
 import ejs from "ejs"
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -10,13 +11,11 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.send('<p>Saludos desde la casa de mis padres </p>');
-})
+// ***********************GET************************
+app.get('/', (req, res) => res.render('home', { ejsTitle: title() }));
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
---
+++ public/javascript/home.js
@@ -0,0 +1,9 @@
+// const _title = title();
+// export { _title as title };
+
+// function title() { return 'Home' }
+//exportamos title
+
+export { title };
+
+const title = () => 'Home';
